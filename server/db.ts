be5cd9bb72e8a9fd99078f60282acc5a3e96892b
rakpa <rakpa8@gmail.com>
2025-04-04 22:18:36 +0200

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -6,7 +6,7 @@ import * as schema from '../shared/schema';
 
 // Using the provided database URL
 const pool = new Pool({
-  connectionString: 'postgresql://rakpa:India%40123@127.0.0.1:5432/postgres?schema=public',
+  connectionString: 'rakpa:abc123@ec2-13-51-114-32.eu-north-1.compute.amazonaws.com:5432/postgres?schema=public',
 });
 
 export const db = drizzle(pool, { schema });
